f6c371455a40c100a420580514514a623c71ca63
IrinaChoulgan <irina_choulgan@icloud.com>
2023-06-22 15:53:05 +0200

    install react
    add favicon, reset style, add container, google fonts

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import "./components/Container/Container"
+import Container from './components/Container/Container';
+
+function App() {
+  return (
+    <div className="App">
+      <Container>Hello!</Container>
+    </div>
+  );
+}
+
+export default App;
